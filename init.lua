da18425a344de46c435173e8e2f3805109e999bb
Thomas Rudin <thomas@rudin.io>
2019-08-19 17:38:48 +0200

    nice looking snow + rain

--- init.lua
+++ init.lua
@@ -8,36 +8,60 @@ dofile(MP.."/register.lua")
 dofile(MP.."/cycle.lua")
 
 
-function do_rain(player)
-	local ppos = player:get_pos()
-
-	minetest.add_particlespawner({
-		amount = 50,
-		time = 5,
-		minpos = vector.add(ppos, {x=-10, y=5, z=10}),
-		maxpos = vector.add(ppos, {x=10, y=5, z=10}),
-		minvel = {x=0, y=-5, z=0},
-		maxvel = {x=0, y=-12, z=0},
-		minacc = {x=0, y=0, z=0},
-		maxacc = {x=0, y=0, z=0},
-		minexptime = 1,
-		maxexptime = 5,
-		minsize = 1,
-		maxsize = 2.7,
-		collisiondetection = true,
-		collision_removal = true,
-		object_collision = true,
-		vertical = true,
-		texture = "weather_rain.png",
-		playername = player:get_player_name()
-	})
-
-end
-
 
 minetest.register_chatcommand("rain_test", {
 	func = function(name)
 		local player = minetest.get_player_by_name(name)
-		do_rain(player)
+		local ppos = player:get_pos()
+
+		minetest.add_particlespawner({
+			amount = 1000,
+			time = 5,
+			minpos = vector.add(ppos, {x=-10, y=10, z=-10}),
+			maxpos = vector.add(ppos, {x=10, y=10, z=10}),
+			minvel = {x=2, y=-5, z=0},
+			maxvel = {x=2, y=-12, z=0},
+			minacc = {x=0, y=0, z=0},
+			maxacc = {x=0, y=0, z=0},
+			minexptime = 1,
+			maxexptime = 5,
+			minsize = 20,
+			maxsize = 30.7,
+			collisiondetection = true,
+			collision_removal = true,
+			object_collision = true,
+			vertical = true,
+			texture = "weather_rain.png",
+			playername = name
+		})
+	end
+})
+
+
+minetest.register_chatcommand("snow_test", {
+	func = function(name)
+		local player = minetest.get_player_by_name(name)
+		local ppos = player:get_pos()
+
+		minetest.add_particlespawner({
+			amount = 1000,
+			time = 5,
+			minpos = vector.add(ppos, {x=-10, y=5, z=-10}),
+			maxpos = vector.add(ppos, {x=10, y=10, z=10}),
+			minvel = {x=0.5, y=-1, z=0},
+			maxvel = {x=1, y=-3, z=0},
+			minacc = {x=0, y=0, z=0},
+			maxacc = {x=0, y=0, z=0},
+			minexptime = 1,
+			maxexptime = 5,
+			minsize = 20,
+			maxsize = 30.7,
+			collisiondetection = true,
+			collision_removal = true,
+			object_collision = true,
+			vertical = true,
+			texture = "weather_snow.png",
+			playername = name
+		})
 	end
 })
--- readme.md
+++ readme.md
@@ -10,3 +10,8 @@ MIT
 - Textures:
   - Snow cover: WTFPL
   - Rain / Snow: CC-BY-SA 3.0, credit goes to TeddyDesTodes, from his weather branch at https://github.com/TeddyDesTodes/minetest/tree/weather
+
+# Previous works
+
+* https://github.com/paramat/snowdrift
+* https://gitlab.com/zombiebot/weather_pack
